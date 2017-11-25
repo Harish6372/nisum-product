0c7660e9d9e175c66abd16a8502bccc97fff9dac
sureharish <sureharish63@gmail.com>
2017-11-25 17:31:10 +0530

    kjdkjf

---
+++ src/main/java/com/nisum/test/demoNisumJerey/DemoNisumJereyApplication.java
@@ -0,0 +1,12 @@
+package com.nisum.test.demoNisumJerey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNisumJereyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNisumJereyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nisum/test/demoNisumJerey/DemoNisumJereyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nisum.test.demoNisumJerey;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoNisumJereyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
